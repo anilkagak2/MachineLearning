ce2cf9c7991b499d16dde2963b8e320f9ab1a151
Anil Kag <anilkagak2@gmail.com>
2016-12-16 23:47:47 +0530

    Initial version of predicting office space prices using only Linear Model and linear features

---
+++ predicting-office-space-price/predictinOfficeSpacePrice.py
@@ -0,0 +1,34 @@
+import numpy as np
+from sklearn import linear_model
+
+FN = raw_input()
+x = FN.split()
+F = int( x[0] )
+N = int( x[1] )
+train_X, train_Y = [], []
+for _ in range(0, N):
+    trainPnt = raw_input()
+    trainPnt = trainPnt.strip().split()
+    features = [ float(x) for x in trainPnt[:F] ]
+    price = float(trainPnt[-1])
+    train_X.append(features)
+    train_Y.append(price)
+
+T = int( raw_input() )
+test_X = []
+for _ in range(0, T):
+    testPnt = raw_input()
+    testPnt = testPnt.strip().split()
+    features = [ float(x) for x in testPnt[:F] ]
+    test_X.append(features)
+    
+#print("Training..")
+## Train the regressor
+model = linear_model.LinearRegression()
+model.fit(np.array(train_X), np.array(train_Y))
+
+#print("Prediction..")
+## Test the regressor
+predictions = model.predict(np.array(test_X))
+for price in predictions:
+    print(price)
